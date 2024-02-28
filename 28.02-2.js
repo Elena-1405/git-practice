b30bd3d9601954cdbd4afa3f9121a81096909e3b
Elena-1405 <helenofspb@gmail.com>
2024-02-28 14:07:54 +0300

    some expers

---
+++ 28.02-2.js
@@ -0,0 +1,14 @@
+const a = [1, 2, 3]
+const b = [1, 2, 3]
+
+console.log(a == b)
+console.log('Hello')
+console.log(a === b)
+console.log([1, 2, 3] == [1, 2, 3])
+console.log([] == +[])
+
+const c = [...a]
+console.log(a == c)
+console.log(b == c)
+console.log(a === c)
+console.log(b === c)
